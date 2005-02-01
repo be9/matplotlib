fb8e0fe95dc2c46aeb200d94519b6f3d1be00455
(no author) <(no author)@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2005-02-01 22:08:25 +0000

    This commit was manufactured by cvs2svn to create branch 'vendor'.

--- matplotlib/lib/matplotlib/numerix/linear_algebra/__init__.py
+++
@@ -1,8 +0,0 @@
-from matplotlib.numerix import which
-
-if which[0] == "numarray":
-    from numarray.linear_algebra import *
-elif which[0] == "numeric":
-    from LinearAlgebra import *
-else:
-    raise RuntimeError("invalid numerix selector")
--- matplotlib/lib/matplotlib/numerix/random_array/__init__.py
+++
@@ -1,8 +0,0 @@
-from matplotlib.numerix import which
-
-if which[0] == "numarray":
-    from numarray.random_array import *
-elif which[0] == "numeric":
-    from RandomArray import *
-else:
-    raise RuntimeError("invalid numerix selector")
